317a6ff86e105c4bbf531857044ee63f7722a6bd
veronicabaicu <veronica_baicu@yahoo.com>
2020-01-31 15:02:46 +0100

    Tema2_exercitiul7

--- src/main/Tema2_ex7.java
+++ src/main/Tema2_ex7.java
@@ -2,6 +2,13 @@ package main;
 
 public class Tema2_ex7 {
 	public static void main(String[]args){
+		int nr = 43;
+		if (nr%2==0)
+			System.out.println(nr+" este numar par");
+			else
+				System.out.println (nr+" este numar impar");
+		
+//daca jumatatea numarului este un numar intreg, atunci numarul este par
 		
 	}
 
