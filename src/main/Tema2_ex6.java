ae033ae7479121cc485eb31db5de0cb745e97900
veronicabaicu <veronica_baicu@yahoo.com>
2020-01-31 14:41:50 +0100

    Tema2_exercitiul6

---
+++ src/main/Tema2_ex6.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Tema2_ex6 {
+	public static void main(String[]args){
+		int a = 6, b = 2, c = 17;
+		int min = a;
+		if (a<b) min = a;
+		else min = b;
+		if (c<min) min = c;
+		else min = b;
+		System.out.println(min);
+		
+//pentru a afla si afisa minimul dintre 3 numere, comparam pe rand 2 numere, apoi minimul lor cu cel de-al treilea
+		
+	}
+}
