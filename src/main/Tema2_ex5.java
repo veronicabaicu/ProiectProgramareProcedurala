23c1ced722680454ae256bb89e352ae48a5e52e3
veronicabaicu <veronica_baicu@yahoo.com>
2020-01-31 14:03:08 +0100

    Tema2_exercitiul5

---
+++ src/main/Tema2_ex5.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Tema2_ex5 {
+	public static void main(String[]args){
+		int i = 1;
+		int f = 1;
+		int n = 13;
+		for(i=1; i<n; i++)
+			while (i < n) {
+				f = f * i;
+				System.out.println("n!=" + f);
+				i++;
+			}
+	}
+
+}
